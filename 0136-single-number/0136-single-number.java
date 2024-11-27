f11dde7cb64265f061e6358b9d38a7e63151eef8
arshuroshan <98870589+arshuroshan@users.noreply.github.com>
2024-11-27 12:54:25 +0530

    Time: 10 ms (23.24%), Space: 45.4 MB (84.64%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.add(num)) {
+                set.remove(num);
+            }
+        }
+        return set.iterator().next();
+    }
+}
