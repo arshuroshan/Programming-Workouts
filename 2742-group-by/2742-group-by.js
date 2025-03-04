8aa1f4f6e974f01afa1e9988214673220e475761
Arshad Roshan <98870589+arshuroshan@users.noreply.github.com>
2025-03-05 00:59:24 +0530

    Time: 99 ms (93.23%), Space: 79.1 MB (7.93%) - LeetHub

---
+++ 2742-group-by/2742-group-by.js
@@ -0,0 +1,14 @@
+Array.prototype.groupBy = function (fn) {
+    return this.reduce((acc, item) => {
+        const key = fn(item);
+        if (!acc[key]) {
+            acc[key] = [];
+        }
+        acc[key].push(item);
+        return acc;
+    }, {});
+};
+
+// Example usage:
+const result = [1, 2, 3].groupBy(String); // {"1":[1],"2":[2],"3":[3]}
+console.log(result);
