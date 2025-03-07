3380ae299b968d8dd4792cbf0125660b05318907
Arshad Roshan <98870589+arshuroshan@users.noreply.github.com>
2025-03-07 14:15:21 +0530

    Time: 154 ms (43.52%), Space: 95.1 MB (5.18%) - LeetHub

---
+++ 2759-flatten-deeply-nested-array/2759-flatten-deeply-nested-array.js
@@ -0,0 +1,14 @@
+var flat = function (arr, n) {
+    if (n <= 0) {
+        return arr;
+    }
+    const ans = [];
+    for (const x of arr) {
+        if (Array.isArray(x) && n > 0) {
+            ans.push(...flat(x, n - 1));
+        } else {
+            ans.push(x);
+        }
+    }
+    return ans;
+};
