739d81dfded0d4a2d94b414318967f18a2b0f134
Arshad Roshan <98870589+arshuroshan@users.noreply.github.com>
2025-08-10 20:34:46 +0400

    Time: 7 ms (72.56%), Space: 18.1 MB (14.1%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -4,15 +4,17 @@ class Solution:
             if s == 0:
                 res.append(path[:])
                 return
-            if i >= len(candidates) or s < candidates[i]:
+            if i >= n or s < candidates[i]:
                 return
-            for j in range(i, len(candidates)):
+            for j in range(i, n):
                 if j > i and candidates[j] == candidates[j - 1]:
                     continue
                 path.append(candidates[j])
                 dfs(j + 1, s - candidates[j])
                 path.pop()
+
         candidates.sort()
+        n = len(candidates)
         res, path = [], []
         dfs(0, target)
         return res
