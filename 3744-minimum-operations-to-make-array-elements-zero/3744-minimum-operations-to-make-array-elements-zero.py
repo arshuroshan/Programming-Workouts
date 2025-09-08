19c346fa4b7e05467d1348a4ad211655fa66a58e
Arshad Roshan <98870589+arshuroshan@users.noreply.github.com>
2025-09-08 11:52:07 +0400

    Time: 4660 ms (5.09%), Space: 55 MB (11.89%) - LeetHub

--- 3744-minimum-operations-to-make-array-elements-zero/3744-minimum-operations-to-make-array-elements-zero.py
+++ 3744-minimum-operations-to-make-array-elements-zero/3744-minimum-operations-to-make-array-elements-zero.py
@@ -1,16 +1,19 @@
 class Solution:
     def minOperations(self, queries: List[List[int]]) -> int:
         def g(x: int) -> int:
-            r, p, i = 0, 1, 1
+            t = 0
+            p = 1
+            i = 1
             while p <= x:
                 c = min(p * 4 - 1, x) - p + 1
-                r += c * i
+                t += c * i
                 i += 1
                 p *= 4
-            return r
-        a = 0
+            return t
+
+        res = 0
         for l, r in queries:
             s = g(r) - g(l - 1)
             m = g(r) - g(r - 1)
-            a += max((s + 1) // 2, m)
-        return a
+            res += max((s + 1) // 2, m)
+        return res
