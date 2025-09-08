14e3fafe092047018609d8d297bb82900c011a45
Arshad Roshan <98870589+arshuroshan@users.noreply.github.com>
2025-09-08 11:53:23 +0400

    Time: 1007 ms (86.85%), Space: 18.3 MB (84.86%) - LeetHub

--- 3277-find-the-number-of-ways-to-place-people-ii/3277-find-the-number-of-ways-to-place-people-ii.py
+++ 3277-find-the-number-of-ways-to-place-people-ii/3277-find-the-number-of-ways-to-place-people-ii.py
@@ -4,7 +4,7 @@ class Solution:
         res = 0
         for i in range(len(points)):
             y1 = points[i][1]
-            m = -inf
+            m = float("-inf")
             for _, y2 in points[i+1:]:
                 if m < y2 <= y1:
                     m = y2
